439d56f6b5275443bfe1c4add4d19ddaa040ebb1
Rovshana <rovshanarzayeva2001@gmail.com>
2022-10-13 10:05:27 +0400

    update

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,21 +1,13 @@
-import { Butik } from "../components/Butik/Butik";
-import { Category } from "../components/categories/Category";
-import { Menu } from "../components/Menu/Menu";
-import Footer from "../components/shared/Footer/Footer";
-import FooterBottom from "../components/shared/Footer/FooterBottom";
-import { HeaderCatagory } from "../components/shared/HeaderCatagory/HeaderCatagory";
-import Navbar from "../components/shared/Navbar/Navbar";
-import SubMenu from "../components/SubMenu/SubMenu";
-import BasketConatiner from "../Features/BasketContainer/BasketConatiner";
+
 import { HomeContainer } from "../Features/HomeContainer/HomeContainer";
-import ProductContainer from "../Features/ProductContainer/ProductContainer";
+
 
 
 export default function Home() {
   return (
     <div >
     
-      {/* <Butik/> */}
+  
      <HomeContainer/>
       
     </div>
